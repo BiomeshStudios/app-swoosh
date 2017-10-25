9d59a31e2105b2cc1d6cd3e8eb3210fc8f98db02
Rob Elser <Rob@biomeshstudios.com>
2017-10-25 16:20:10 +0100

    Button Code
    Created several IBAction functions for buttons in LeagueVC.swift

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,20 +9,43 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
     @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        player = Player()
     }
+    
+    /*
+    // MARK: - Button Code
+    */
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womans")
+    }
+
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -32,5 +55,7 @@ class LeagueVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    
 
 }
