657a9e4ba9ec434422ef7843225b4f1168d6bd22
Rob Elser <Rob@biomeshstudios.com>
2017-10-25 07:53:16 +0100

    Created Additional VC files
    Renamed ViewController.swift to WelcomeVC.swift and created LeagueVC.swift and SkillVC.swift. Then connected these to the relevant View Controllers in the Main.storyboard.

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeVC.swift
 //  app-swoosh
 //
 //  Created by Robert Elser on 24/10/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
